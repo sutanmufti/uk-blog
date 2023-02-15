c4d2f70806763cc5aa99226480b0761f31e6a697
sutan mufti <upsutan@gmail.com>
2023-02-15 23:21:43 +0000

    featured

--- src/lib/components/Features.svelte
+++ src/lib/components/Features.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <!-- <Maptooltip/> -->
-
+<div class='main'>
+    Featured Topics:
+</div>
 <div class='main'>
     <FeatureCard title='Student Visa' content="Are you a student coming to the UK? Look here and view some stories other students have gone through."/>
     <FeatureCard title='Working Visa' content="Working in the UK requires a sponsor letter from the hiring company. Click here for some Indonesian expats stories of applying for Tier 2 visa."/>
