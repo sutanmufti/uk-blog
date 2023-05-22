5fa92ab820a6f3c27caa18f808d50a8ae71bf8da
sutan mufti <upsutan@gmail.com>
2023-05-23 00:17:00 +0100

    multiple pages blog

---
+++ src/routes/blog/[page]/+page.server.ts
@@ -0,0 +1,62 @@
+import { error } from '@sveltejs/kit';
+import { APIKEY, LIST,CUSTOMFIELD_LOCATION,CUSTOMFIELD_IMAGE } from '$env/static/private';
+import { getTasksByPage } from '../../story/[story]/Story';
+import type { CustomFieldLocation,CustomFieldImage, Tasks, } from '../../story/[story]/SampleStory';
+
+export async function load({params}) {
+    const pagenum = Number(params.page)
+    if (!pagenum && pagenum != 0) throw error(404, 'Not found');
+
+    const posts: {tasks: Tasks[], last_page: boolean} = await getTasksByPage(LIST,Number(params.page),APIKEY);
+
+
+    console.log(posts)
+    if(!posts.last_page) throw error(404, 'Not found');
+
+
+    const stories = posts.tasks.map(d=> {
+        
+        const location = <CustomFieldLocation[]> d.custom_fields.filter(d=>d.id===CUSTOMFIELD_LOCATION && d.type=='location')
+        const image = <CustomFieldImage[]>d.custom_fields.filter(d=>d.id===CUSTOMFIELD_IMAGE && d.type=='attachment')
+
+        const imageExtract = image[0].value?.map(y=>{
+
+
+            const {
+                // thumbnail_large,
+                thumbnail_medium,
+                // thumbnail_small,
+                url} = y
+            return {
+                thumbnail: thumbnail_medium,
+                url: url
+            }
+        })
+
+        const locationfield = (location[0]) ? location[0].value?.location : {lat: undefined, lng: undefined}
+        // console.log("is location?")
+        return {
+            id: d.id,
+            title: d.name,
+            description: d.description,
+            location: locationfield,
+            time: Number(d.date_created),
+            image: imageExtract
+    }})
+
+
+    
+
+    
+    
+   if (posts){
+    return {
+        status: 200,
+        data: stories,
+        posts: posts,
+        last_page: posts.last_page
+    }
+   }
+
+    throw error(404, 'Not found');
+}
---
+++ src/routes/blog/[page]/+page.svelte
@@ -0,0 +1,43 @@
+<script lang='ts'>
+
+
+    export let data;
+    import Story from '../Story.svelte';
+
+    const stories = data.data
+    
+    const posts = data.posts
+
+
+    
+
+    const titles = stories.map(d=>d.title)
+    
+
+
+</script>
+
+<div class='stories'>
+    <div class='contents'>
+        {#each stories as t,i}
+            <Story title={t.title} description={t.description} lat={t.location?.lat} long={t.location?.lng} image={t.image} created={t.time} storyid={t.id}/>
+        
+        {/each}
+    </div>
+</div>
+
+
+<!-- {JSON.stringify(data.posts)} -->
+
+<style>
+    .stories{
+        /* border: 1px solid red; */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .contents {
+        width: 1000px;
+    }
+</style>
