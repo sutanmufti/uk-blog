8f3bf62499b6831fff8c044d9270a6d7d171af55
sutan mufti <upsutan@gmail.com>
2023-05-24 08:25:50 +0100

    url

--- src/routes/story/[story]/+page.server.ts
+++ src/routes/story/[story]/+page.server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
-import { APIKEY, CUSTOMFIELD_STORY } from '$env/static/private';
+import { APIKEY, CUSTOMFIELD_STORY,CUSTOMFIELD_IMAGE } from '$env/static/private';
 import { GetStory,getComments } from './Story';
-import type { Story, StorySuccess, Comment } from './SampleStory';
+import type { Story, StorySuccess, Comment, CustomFieldImage } from './SampleStory';
 
 export async function load({ params, url }) {
     const refresh = url.searchParams.get("refresh")
@@ -9,7 +9,25 @@ export async function load({ params, url }) {
 
     const post: Story = await GetStory(params.story,APIKEY, refreshredis);
     const comments: Comment = await getComments(params.story,APIKEY,refreshredis)
+    const image = <CustomFieldImage[]>post.custom_fields.filter(d=>d.id===CUSTOMFIELD_IMAGE && d.type=='attachment')
 
+    // console.log('comments:',comments)
+    console.log('thecomments:',comments.comments.map(d=>d.comment.map(p=>p.attachment)))
+    const imageExtract = image[0].value?.map(y=>{
+
+
+        const {
+            // thumbnail_large,
+            thumbnail_medium,
+            // thumbnail_small,
+            url} = y
+        return {
+            thumbnail: thumbnail_medium,
+            url: url
+        }
+    })
+
+    const imgurl = (imageExtract) ? imageExtract[0].url : '';
     
     if (post.err){
         throw error(404, 'Not found');
@@ -25,7 +43,8 @@ export async function load({ params, url }) {
             status: 200,
             name: title,
             story:  comments,
-            post : post
+            post : post,
+            img : imgurl
         }
     }
 
